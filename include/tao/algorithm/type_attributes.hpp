52c26892c2cd19b37026d0975507b7cabba6bc60
Fernando Pelliccioni <fpelliccioni@gmail.com>
2016-04-28 01:36:47 -0300

    Type attributes file

---
+++ include/tao/algorithm/type_attributes.hpp
@@ -0,0 +1,29 @@
+//! \file tao/algorithm/type_attributes.hpp
+// Tao.Algorithm
+//
+// Copyright Fernando Pelliccioni, 2016
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+
+#ifndef TAO_ALGORITHM_TYPE_ATTRIBUTES_HPP
+#define TAO_ALGORITHM_TYPE_ATTRIBUTES_HPP
+
+#include <iterator>
+
+#include <tao/algorithm/concepts.hpp>
+
+// Iterators
+template <Iterator I>
+using ValueType = typename std::iterator_traits<I>::value_type;
+
+template <Iterator I>
+using IteratorCategory = typename std::iterator_traits<I>::iterator_category;
+
+// Containers
+template <Container C>
+using SizeType = typename C::size_type;
+
+
+#endif //TAO_ALGORITHM_TYPE_ATTRIBUTES_HPP
