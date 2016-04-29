d90f01027b1576d84a2997dd657eb0d62ab0477a
Fernando Pelliccioni <fpelliccioni@gmail.com>
2016-04-29 14:18:54 -0300

    fix test

--- test/shift_right.cpp
+++ test/shift_right.cpp
@@ -1,6 +1,6 @@
 // Tao.Algorithm
 //
-// Copyright Fernando Pelliccioni, 2016
+// Copyright Fernando Pelliccioni 2016
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -18,25 +18,31 @@ using namespace std;
 using namespace tao::algorithm;
 
 template <Iterator I, Integral N>
-void check(I first, I last, N n, ValueType<I> const& expected_value) {
+void check(I first, I last, N n, bool expected_empty, ValueType<I> const& expected_value) {
     auto m = shift_right(first, last, n);
-    REQUIRE(*m == expected_value);
-    REQUIRE(&*m == &*next(first, n));
+
+    if (m == last) {
+        REQUIRE(expected_empty);
+    } else {
+        REQUIRE(!expected_empty);
+        REQUIRE(*m == expected_value);
+        REQUIRE(&*m == &*next(first, n));
+    }
 }
 
 TEST_CASE("shift_right_forward", "shift_right on ForwardIterator's") {
 
     forward_list<int> c = { 1, 2, 3, 4, 5, 6, 7, 8 };
 
-    check(begin(c), end(c), 0, 1);
-    check(begin(c), end(c), 1, 1);
-    check(begin(c), end(c), 2, 1);
-    check(begin(c), end(c), 3, 1);
-    check(begin(c), end(c), 4, 1);
-    check(begin(c), end(c), 5, 1);
-    check(begin(c), end(c), 6, 1);
-    check(begin(c), end(c), 7, 1);
-    check(begin(c), end(c), 8, 1);
+    check(begin(c), end(c), 0, false, 1);
+    check(begin(c), end(c), 1, false, 1);
+    check(begin(c), end(c), 2, false, 1);
+    check(begin(c), end(c), 3, false, 1);
+    check(begin(c), end(c), 4, false, 1);
+    check(begin(c), end(c), 5, false, 1);
+    check(begin(c), end(c), 6, false, 1);
+    check(begin(c), end(c), 7, false, 1);
+    check(begin(c), end(c), 8, true, 0);
 
 
 
