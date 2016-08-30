df8aeccc467724bc7e3a82aaafd459fbc21ad2e1
fpelliccioni <fpelliccioni@gmail.com>
2016-08-30 16:39:58 -0300

    tabs to spaces

--- include/tao/algorithm/counter_machine.hpp
+++ include/tao/algorithm/counter_machine.hpp
@@ -18,55 +18,55 @@
 namespace tao { namespace algorithm {
 
 template <ForwardIterator I, BinaryOperation Op>
-// 	TODO: requires Op is associative
-	requires(Mutable<I> && ValueType<I> == Domain<Op>) 
+//  TODO: requires Op is associative
+    requires(Mutable<I> && ValueType<I> == Domain<Op>) 
 ValueType<I> add_to_counter(I f, I l, Op op, ValueType<I> x, ValueType<I> const& e) {
-	// precondition: x != e
-	while (f != l) {
-		if (*f == e) {
-			*f = x;
-			return e;
-		}
-		x = op(*f, x);
-		*f = e;
-		++f;
-	}
-	return x;
+    // precondition: x != e
+    while (f != l) {
+        if (*f == e) {
+            *f = x;
+            return e;
+        }
+        x = op(*f, x);
+        *f = e;
+        ++f;
+    }
+    return x;
 }
 
 template <Semiregular T, BinaryOperation Op, std::size_t Size = 64>
-// 	requires Op is associative //TODO
-	requires(Domain<Op> == T) //TODO...
+//  requires Op is associative //TODO
+    requires(Domain<Op> == T) //TODO...
 struct counter_machine {
-	counter_machine(Op op, T const& e)
-		: op(op), e(e), l(f)
-	{}
+    counter_machine(Op op, T const& e)
+        : op(op), e(e), l(f)
+    {}
 
-	counter_machine(counter_machine const&) = delete;
-	counter_machine& operator=(counter_machine const&) = delete;
-	
-	//Move Constructor and Move Assignment Operator are deleted too
+    counter_machine(counter_machine const&) = delete;
+    counter_machine& operator=(counter_machine const&) = delete;
+    
+    //Move Constructor and Move Assignment Operator are deleted too
     //See http://stackoverflow.com/questions/37092864/should-i-delete-the-move-constructor-and-the-move-assignment-of-a-smart-pointer/38820178#38820178
     // and http://talesofcpp.fusionfenix.com/post-24/episode-eleven-to-kill-a-move-constructor
 
-	void add_to(T x, T* to) {
-		// precondition: TODO
-		x = add_to_counter(to, l, op, x, e);
-		if (x != e) {
-			*l = x;
-			++l;
-		}
-	}
+    void add_to(T x, T* to) {
+        // precondition: TODO
+        x = add_to_counter(to, l, op, x, e);
+        if (x != e) {
+            *l = x;
+            ++l;
+        }
+    }
 
-	void add(T x) {
-		// precondition: must not be called more than 2^Size - 1 times
+    void add(T x) {
+        // precondition: must not be called more than 2^Size - 1 times
         add_to(x, f);
-	}
+    }
 
-	const Op op;
-	const T e;
-	T f[Size];
-	T* l;
+    const Op op;
+    const T e;
+    T f[Size];
+    T* l;
 };
 
 }} /*tao::algorithm*/
