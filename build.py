fda8d324e2794c8312c4d246d4ca9f11bc8e8961
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-11 18:59:39 -0300

    just release builds

--- build.py
+++ build.py
@@ -1,11 +1,10 @@
 import os
 from conan.packager import ConanMultiPackager
-# from ci_utils import option_on_off, march_conan_manip, pass_march_to_compiler, glibcxx_supports_cxx11_abi
 from ci_utils import get_builder
 
 if __name__ == "__main__":
     # builder = ConanMultiPackager(username="fpelliccioni", channel="stable",
-    #                              remotes="https://api.bintray.com/conan/tao-cpp/tao-cpp"
+    #                              remotes="https://api.bintray.com/conan/tao-cpp/tao"
     #                              )
 
     builder, name = get_builder(os.path.dirname(os.path.abspath(__file__)))
@@ -13,7 +12,8 @@ if __name__ == "__main__":
 
     filtered_builds = []
     for settings, options, env_vars, build_requires in builder.builds:
-        filtered_builds.append([settings, options, env_vars, build_requires])
+        if settings["build_type"] == "Release":
+            filtered_builds.append([settings, options, env_vars, build_requires])
 
     builder.builds = filtered_builds
     builder.run()
